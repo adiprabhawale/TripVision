3ca2f234e1e5c3d779e2f8aa086dd6ab5cdda890
Aditya <adiprabhawale@gmail.com>
2026-04-02 23:23:40 +0530

    adding analytics fucntions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css';
 import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { AuthProvider } from '@/components/auth-provider';
 
@@ -30,6 +32,8 @@ export default function RootLayout({
         <AuthProvider>
           {children}
           <Toaster />
+          <Analytics />
+          <SpeedInsights />
         </AuthProvider>
       </body>
     </html>
